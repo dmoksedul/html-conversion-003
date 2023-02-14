94409c18545c6eb4b68499b850a5f4691465b699
dmoksedul <dmoksedulislam@gmail.com>
2023-02-14 11:03:13 +0600

    add website

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+function preloader(){
+  document.querySelector(".preloader").classList.add("preloaded");
+}
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+      })
+      $('.testimonal_carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            }
+        }
+      })
+})
+
